402a9a65363eb20c72996f9ba02a5ca652c142b8
Ian Cox <ian@iancox.com>
2021-01-09 18:35:51 -0500

    messing around with shoppify API

--- site/plugins/kirby-shopify/src/models/shopify.products.php
+++ site/plugins/kirby-shopify/src/models/shopify.products.php
@@ -29,7 +29,7 @@ class ShopifyProductsPage extends Page
                 'title'                       => $product['title'],
                 'shopifyTitle'                => $product['title'],
                 'shopifyID'                   => $product['id'],
-                'shopifyStatus'               => $product['status'],
+                'shopifyStatus'               => Yaml::encode($product),
                 'shopifyCreatedAt'            => $product['created_at'],
                 'shopifyUpdatedAt'            => $product['updated_at'],
                 'shopifyPublishedAt'          => $product['published_at'],
